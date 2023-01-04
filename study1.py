bebc1cfb7c9c4f8494544eaddfa869e75e0490cb
liuzongyue <2559221072@qq.com>
2023-01-04 17:38:55 +0800

    字符练习

---
+++ study1.py
@@ -0,0 +1,13 @@
+print("hello, world")
+print(ord("A"))
+print(ord("种"))
+print(chr(2590))
+print("\u4e2d\u6587")
+a = b"abc"
+
+print(a)
+
+print("ABC".encode('ascii'))
+print("中文".encode('utf-8'))
+
+print(len("虎骨VB"))
